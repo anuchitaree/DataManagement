9b33c115c1064a5de19708dae0a1debbab05d0db
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2023-01-23 14:11:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using DataManagement;
+
+namespace Company.WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ Workers/ScheduleClean.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagement.Workers
+{
+    public class ScheduleClean : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+
+        private string _destinationPath1=null!;
+        private string _destinationPath2 = null!;
+        private int _dayDelete = 0;
+        private Boolean _isDelete = false;
+
+
+        public ScheduleClean(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Initialize();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void Initialize()
+        {
+            _destinationPath1 =_configuration.GetValue<string>("DestinationPath:Path1");
+            _destinationPath2 = _configuration.GetValue<string>("DestinationPath:Path2");
+            _dayDelete = _configuration.GetValue<int>("DayDelete");
+            _isDelete = _configuration.GetValue<bool>("isDeleted");
+
+           
+
+            
+        }
+    }
+}
